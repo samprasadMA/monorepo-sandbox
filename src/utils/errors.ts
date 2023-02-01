2eeb7914b997ef4965bb520d1fdb15bcd3bbedcb
Sam Prasad <sam.prasad@moodys.com>
2023-01-31 23:58:05 -0500

    feat: v1 of node API

---
+++ src/utils/errors.ts
@@ -0,0 +1,7 @@
+export const invalidRepoObject = "repo invalid or not found";
+
+export const invalidPRObject = "PR number invalid or not found";
+
+export const invalidRepoAndPRObject = "Repo or Owner invalid or not found" ;
+
+export const noOpenPRsFound = "No open PRs found";
